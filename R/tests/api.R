af2b4ecd2cd821d62b03045023e01ef9811c247a
Karel A. Kroeze <k.a.kroeze@utwente.nl>
2022-03-07 17:23:41 +0100

    add api test scaffold

---
+++ R/tests/api.R
@@ -0,0 +1,33 @@
+library(httr)
+setwd("..")
+source("functions/mirtCAT_v2.R")
+
+# assume api is running on localhost
+HOST <- "http://localhost"
+PORT <- 4321
+
+# live test version
+# HOST <- "https://cat-api.home.karel-kroeze.nl"
+# PORT <- NULL
+
+PRIOR <- 0
+PATTERN <- data.frame(
+  item_id = c(3,8),
+  response = c(2,1)
+)
+
+# get estimate and next_item from API
+response <- httr::POST(
+  HOST, port = PORT, path = "next_item", httr::content_type("application/json"),
+  body = list(
+    prior = PRIOR,
+    administered = PATTERN),
+  encode = "json")
+httr::content(response)
+
+# get estimate 'directly' (ish)
+data <- setup_cat_data()
+.prior <- set_prior(PRIOR)
+cat <- run_cat(data, .prior, PATTERN)
+cat$person$thetas
+mirtCAT::findNextItem(cat)
